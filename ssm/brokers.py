e763f57785da52e4cb3e21fc265066d9be8e58be
RedProkofiev <nicholaswhyatt@gmail.com>
2023-10-06 14:54:45 +0000

    Licensing

--- ssm/brokers.py
+++ ssm/brokers.py
@@ -1,23 +1,22 @@
-"""
-   Copyright (C) 2012 STFC.
+#!/usr/bin/env python
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+# Copyright 2012 UK Research and Innovation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-       http://www.apache.org/licenses/LICENSE-2.0
+"""Class to interact with a BDII LDAP server to retrieve information about
+the stomp brokers specified in a network.  Soon to be deprecated."""
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-   @author: Will Rogers
-
-Class to interact with a BDII LDAP server to retrieve information about
-the stomp brokers specified in a network.
-"""
 from __future__ import print_function
 
 import ldap
--- ssm/crypto.py
+++ ssm/crypto.py
@@ -1,25 +1,24 @@
-"""
-   Copyright (C) 2012 STFC.
+#!/usr/bin/env python
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+# Copyright 2021 UK Research and Innovation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-   @author: Kevin Haines, Will Rogers
-
-   The crypto module calls openssl command line directly, using subprocess.
+"""The crypto module calls openssl command line directly, using subprocess.
    We investigated python's crypto libraries (all openssl bindings) and
    found that none were mature enough to implement the SMIME crypto we had
-   decided on.
-"""
+   decided on."""
+
 from __future__ import print_function
 
 from subprocess import Popen, PIPE
